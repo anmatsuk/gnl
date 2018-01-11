f595824c0ba079078a7ade79a0131d3bebdd0a59
anmatsuk <matsuknastya@gmail.com>
2018-01-11 15:47:44 -0800

    final

--- get_next_line.c
+++ get_next_line.c
@@ -4,18 +4,21 @@ static void ft_strextend(char **line, char *tmp)
 {
 	size_t	line_len;
 	char 	*line_copy;
-	int	i;
-	int	j;
+	size_t	i;
+	int		j;
 	
 	line_len = 0;
 	if (*line)
 		line_len = ft_strlen(*line);
-	i = -1;
+	i = 0;
 	j = -1;
 	line_copy = (char*)malloc(sizeof(char) * (BUFF_SIZE + line_len + 1));
 	*(line_copy + line_len + BUFF_SIZE) = '\0';
-	while (++i < line_len)
+	while (i < line_len)
+	{
 		*(line_copy + i) = *(*line + i);
+		i++;
+	}
 	free(*line);
 	while (++j < BUFF_SIZE)
 	{
@@ -38,29 +41,35 @@ static int	read_from_file(const int fd, char **line)
 		return (1);
 	}
 	*(tmp) = '\0';
-	ft_strextend(&(line[fd]), tmp);
+	if (!line[fd])
+		ft_strextend(&(line[fd]), tmp);
 	free(tmp);
 	return (code);
 }
 
-static char *parse_line(char **line, int i)
+static char *parse_line(char **line, size_t i)
 {
-	int		j;
+	size_t	j;
 	char	*result;
 	char	*copy;
 
-	j = -1;
+	j = 0;
 	result = (char*)malloc(sizeof(char) * (i + 1));
 	*(result + i) = '\0';
-	while (++j < i)
+	while (j < i)
+	{
 		*(result + j) = *(*line + j);
+		j++;
+	}
 	copy = (char*)malloc(sizeof(char) * (ft_strlen(*line) - i + 1));
-	*(copy + ft_strlen(*line) - i) = '\0';
+	*(copy + ft_strlen(*line) - i - 1) = '\0';
 	j = 0;
-	while(++i < ft_strlen(*line))
+	i++;
+	while(i < ft_strlen(*line))
 	{
 		*(copy + j) = *(*line + i);
 		j++;
+		i++;
  	}
  	free(*line);
  	*line = copy;
@@ -69,40 +78,44 @@ static char *parse_line(char **line, int i)
 
 static char	*get_line(const int fd, char **lines, int *code)
 {
-	char	*line;
-	size_t	len;
 	int		i;
 
-	if (!*(lines + fd) || ft_strlen(lines[fd]) == 0)
-		*code = read_from_file(fd, lines);
-	if (*code == -1)
-		return (NULL);
-	if (*code == 0 && ft_strlen(lines[fd]) == 0)
-		return (NULL);
-	i = 0;
-	while (lines[fd][i] != '\n' && lines[fd][i] != '\0')
-		i++;
-	if (lines[fd][i] == '\n')
-		return (parse_line((lines + fd), i));
-	else
-		*code = read_from_file(fd, lines);
-	return (get_line(fd, lines, code));	
+    if (!(lines[fd]))
+        *code = read_from_file(fd, lines);
+    i = 0;
+    if (lines[fd][0] != '\0')
+    	*code = 1;
+    else
+    	*code = read_from_file(fd, lines);
+    while (*code == 1 && lines[fd][i] != '\n')
+    {
+        if (lines[fd][i] == '\0')
+        {
+         	//return (get_line(fd, lines, code));
+            *code = read_from_file(fd, lines);
+            i--;
+        }
+        i++;
+    }
+    return (parse_line((lines + fd), i));
 }
 
 int			get_next_line(const int fd, char **line)
 {
 	static char **lines;
-	char		*result;
 	int			code;
 
 	code = 0;
-	if (fd < 1 || fd > FD_LIMIT || !line || BUFF_SIZE < 1)
+	if (fd < 0 || fd > FD_LIMIT || !line || BUFF_SIZE < 1)
 		return (-1);
 	if (!lines)
 		lines = (char**)malloc(sizeof(char*) * (FD_LIMIT + 1));
 	*line = get_line(fd, lines, &code);
-	if (code == 0 && !(*line))
+	if (code == 0 && ft_strlen(*line) == 0)
+	{
+		*line = NULL;
 		return (0);
+	}
 	if (code == -1)
 		return (-1);
 	return (1);
